4c4c9f6db05ef53a6017b34219cf58f01d837989
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-02-05 13:01:30 -0500

    feat : bloc4 flipbit.

--- client.py
+++ client.py
@@ -5,9 +5,18 @@ from pathlib import Path
 HOST = "127.0.0.1"
 PORT = 12345
 
+def flip_first_bit(data: bytes):
+    if len(data) == 0:
+        return data
+    ba = bytearray(data) 
+    ba[0] == 0b00000001 
+    return bytes(ba)
+
 msg = Path("data/message.txt").read_text(encoding="utf-8").encode("utf-8")
 h = hashlib.sha256(msg).hexdigest() 
-payload = msg + b"\x00" + h.encode("ascii")
+corrupt_msg = flip_first_bit(msg)
+
+payload = corrupt_msg + b"\x00" + h.encode("ascii")
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
